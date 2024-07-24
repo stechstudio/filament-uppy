81865ca2892fa53b935e14ae74603bba5aed9800
David Palmer <dp88@users.noreply.github.com>
2024-07-24 11:06:57 -0400

    register a companion instance
    would be nice to avoid this though?

--- src/UppyUploader.php
+++ src/UppyUploader.php
@@ -4,6 +4,7 @@ namespace STS\FilamentUppy;
 
 use Closure;
 use Filament\Forms\Components\Field;
+use STS\LaravelUppyCompanion;
 
 class UppyUploader extends Field
 {
@@ -13,6 +14,8 @@ class UppyUploader extends Field
 
     protected Closure|string $deleteEndpoint = '';
 
+    protected Closure|LaravelUppyCompanion|string|null $companion;
+
     protected string $view = 'filament-uppy::uppy-uploader';
 
     public function endpoints(Closure|string $upload, Closure|string $success = '', Closure|string $delete = ''): static
@@ -20,6 +23,13 @@ class UppyUploader extends Field
         $this->uploadEndpoint = $upload;
         $this->successEndpoint = $success;
         $this->deleteEndpoint = $delete;
+
+        return $this;
+    }
+
+    public function companion(Closure|LaravelUppyCompanion|string $companion): static
+    {
+        $this->companion = $companion;
 
         return $this;
     }
@@ -37,5 +47,28 @@ class UppyUploader extends Field
     public function getDeleteEndpoint(): string
     {
         return $this->evaluate($this->deleteEndpoint);
+    }
+
+    public function getCompanion(): LaravelUppyCompanion
+    {
+        // If no companion was provided, resolve the default companion.
+        if ($this->companion === null) {
+            return app(LaravelUppyCompanion::class);
+        }
+
+        // Resolve the companion from the provided value.
+        $evaluated = $this->evaluate($this->companion);
+
+        // If it's a string, it should refer to a specific LaravelUppyCompanion singleton.
+        if (is_string($evaluated) && $evaluated !== '') {
+            $evaluated = app($evaluated);
+        }
+
+        // If it's a LaravelUppyCompanion instance, return it.
+        if ($evaluated instanceof LaravelUppyCompanion) {
+            return $evaluated;
+        }
+
+        throw new \Exception('Invalid companion provided');
     }
 }
