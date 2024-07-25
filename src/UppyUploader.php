0b733fc9b1ecb612800310a1b1fa533ffe34a0ab
David Palmer <dp88@users.noreply.github.com>
2024-07-25 10:51:48 -0400

    remove companion from component config

--- src/UppyUploader.php
+++ src/UppyUploader.php
@@ -2,10 +2,8 @@
 
 namespace STS\FilamentUppy;
 
-use Aws\S3\S3ClientInterface;
 use Closure;
 use Filament\Forms\Components\Field;
-use STS\LaravelUppyCompanion\LaravelUppyCompanion;
 
 class UppyUploader extends Field
 {
@@ -15,8 +13,6 @@ class UppyUploader extends Field
     protected Closure|string $successEndpoint = '';
 
     protected Closure|string $deleteEndpoint = '';
-
-    protected Closure|LaravelUppyCompanion|string $companion = 'laravel-uppy-companion.filament-uppy';
 
     protected string $view = 'filament-uppy::uppy-uploader';
 
@@ -45,39 +41,6 @@ class UppyUploader extends Field
         return $this;
     }
 
-    /**
-     * Explicitly provide a LaravelUppyCompanion instance or a string reference to a singleton.
-     *
-     * @param Closure|LaravelUppyCompanion|string $companion
-     * @return $this
-     */
-    public function companion(Closure|LaravelUppyCompanion|string $companion): static
-    {
-        $this->companion = $companion;
-
-        return $this;
-    }
-
-    /**
-     * Manually configure the default or provided LaravelUppyCompanion instance.
-     *
-     * @param Closure|string $bucket The S3 bucket name or a callback to resolve it.
-     * @param Closure|S3ClientInterface $client The S3 client instance or a callback to resolve it.
-     * @param Closure|null $key A callback to resolve the key for the uploaded file. (optional)
-     * @return $this
-     * @throws \Exception
-     */
-    public function configureCompanion(Closure|string $bucket, Closure|S3ClientInterface $client, ?Closure $key = null): static
-    {
-        $this->getCompanion()->configure(
-            $this->evaluate($bucket),
-            $this->evaluate($client),
-            $key
-        );
-
-        return $this;
-    }
-
     public function getUploadEndpoint(): string
     {
         return $this->evaluate($this->uploadEndpoint);
@@ -91,23 +54,5 @@ class UppyUploader extends Field
     public function getDeleteEndpoint(): string
     {
         return $this->evaluate($this->deleteEndpoint);
-    }
-
-    public function getCompanion(): LaravelUppyCompanion
-    {
-        // Resolve the companion from the provided value.
-        $evaluated = $this->evaluate($this->companion);
-
-        // If it's a string, it should refer to a specific LaravelUppyCompanion singleton.
-        if (is_string($evaluated) && $evaluated !== '') {
-            $evaluated = app($evaluated);
-        }
-
-        // If it's a LaravelUppyCompanion instance, return it.
-        if ($evaluated instanceof LaravelUppyCompanion) {
-            return $evaluated;
-        }
-
-        throw new \Exception('Invalid companion provided');
     }
 }
