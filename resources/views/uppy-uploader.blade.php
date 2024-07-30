837da12b50a7a98b60bb5a57730a30953f32ea82
David Palmer <dp88@users.noreply.github.com>
2024-07-30 11:44:52 -0400

    show errors on uploads

--- resources/views/uppy-uploader.blade.php
+++ resources/views/uppy-uploader.blade.php
@@ -74,7 +74,14 @@
                             </td>
 
                             <td class="px-3 py-2 text-sm text-gray-700 dark:text-gray-200">
-                                <span x-text="humanReadableFilesize(file.size)"></span>
+                                <span
+                                    x-show="!file.error"
+                                    x-text="humanReadableFilesize(file.size)"
+                                ></span>
+                                <span
+                                    x-show="!!file.error"
+                                    class="text-red-500"
+                                >Error</span>
                             </td>
 
                             <td class="px-3 py-2 text-sm text-gray-700 dark:text-gray-200">
